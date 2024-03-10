b81e16fe0b56a7d944b92720cc9fb9d09dae7174
msdsn <devsanms@outlook.com>
2024-03-11 00:18:49 +0300

    Backend başlangıç

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'api',
+
+    # third-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
