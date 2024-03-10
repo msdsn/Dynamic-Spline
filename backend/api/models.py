a5d0dc893160a8b82d976605809b72592f9b7e3e
msdsn <devsanms@outlook.com>
2024-03-11 00:54:39 +0300

    backend yaz, ürün ekleme, ürünleri listeleme

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerializer
